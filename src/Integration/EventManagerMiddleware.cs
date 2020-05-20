be4d48cd261c7975ee4fb5cca1bf159a8f576e63
Andrea Tupini <tupini07@gmail.com>
2020-05-20 20:53:22 +0000

    add example project

---
+++ src/Integration/EventManagerMiddleware.cs
@@ -0,0 +1,44 @@
+// using Microsoft.AspNetCore.Builder;
+
+
+// namespace Integration
+// {
+//     public static class EventManagerMiddleware
+//     {
+//         public static IApplicationBuilder UseEventManager(this IApplicationBuilder builder)
+//         {
+//             return builder.UseMiddleware<EventManagerMiddleware>();
+//         }
+//     }
+// }
+
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SwaggerUIBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerUI(
+            this IApplicationBuilder app,
+            Action<SwaggerUIOptions> setupAction = null)
+        {
+            var options = new SwaggerUIOptions();
+            if (setupAction != null)
+            {
+                setupAction(options);
+            }
+            else
+            {
+                options = app.ApplicationServices.GetRequiredService<IOptions<SwaggerUIOptions>>().Value;
+            }
+
+            app.UseMiddleware<SwaggerUIMiddleware>(options);
+
+            return app;
+        }
+    }
+}
