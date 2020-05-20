596bf2ca1ce3fa2f4cbf0a0532c43bc29a059ae3
Andrea Tupini <tupini07@gmail.com>
2020-05-20 21:32:33 +0000

    update target and userMiddleware name

--- Integration/EventManagerMiddlewareExtensions.cs
+++ Integration/EventManagerMiddlewareExtensions.cs
@@ -6,11 +6,10 @@ namespace Integration
 {
     public static class EventManagerMiddlewareExtensions
     {
-        public static IApplicationBuilder UseEventManager(this IApplicationBuilder builder)
+        public static IApplicationBuilder UseCecEventManager(this IApplicationBuilder builder)
         {
             // see here for reference: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/e33dba2e416469fca6c2f4975c1c41dbac942804/src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIBuilderExtensions.cs
                 return builder.UseMiddleware<EventManagerMiddleware>();
-            
         }
     }
 }
