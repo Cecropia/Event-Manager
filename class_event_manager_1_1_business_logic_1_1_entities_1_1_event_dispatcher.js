60910fe08b857503d6e2636efa11c6f33f98189e
tupini07 <tupini07@users.noreply.github.com>
2020-10-23 17:11:53 +0000

    deploy: 0afa43fa6756f10ab42f272f03528bede7335834

--- class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.js
+++ class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.js
@@ -3,7 +3,7 @@ var class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher =
     [ "Dispatch", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#a67a3a4b48b34fd720a1a26eaef5eae4e", null ],
     [ "Register", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#a5e88a4223e1ee8887e3073872cd92d73", null ],
     [ "RegisterLocal", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#a25d3e9cbc41292e7ed65a615c8a8240b", null ],
-    [ "SimpleDispatch", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#afe169f76ebb23492a4c678d86bff83c5", null ],
+    [ "SimpleDispatch", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#a930c00c47b2d8fe67282d8822f303241", null ],
     [ "customAuthProviderRegistry", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#ad42b5a8502ab229cb550ac09f5bbb1da", null ],
     [ "Instance", "class_event_manager_1_1_business_logic_1_1_entities_1_1_event_dispatcher.html#a8335a476b6f739eb12fff88037cc202a", null ]
 ];
